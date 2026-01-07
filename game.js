7abf0acd7d90bd631da246829a38d33a727b60ec
Tyler Odom <tylerodom@Mac.attlocal.net>
2026-01-07 11:25:46 -0500

    added controls and fullscreen for mobile

--- game.js
+++ game.js
@@ -9,6 +9,10 @@ const config = {
             debug: false
         }
     },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
     scene: {
         preload: preload,
         create: create,
@@ -19,6 +23,10 @@ const config = {
 const game = new Phaser.Game(config);
 
 let player, stars, bombs, platforms, cursors, score = 0, scoreText;
+let portraitOverlay;
+let leftButton, rightButton, jumpButton;
+let leftText, rightText, jumpText;
+let isMobile = false;
 
 function preload() {
     this.load.image('sky', 'assets/sky.png');
@@ -29,6 +37,12 @@ function preload() {
 }
 
 function create() {
+    // Reliable mobile detection
+    isMobile = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+
+    // Auto fullscreen
+    this.scale.startFullscreen();
+
     this.add.image(400, 300, 'sky');
 
     platforms = this.physics.add.staticGroup();
@@ -77,6 +91,105 @@ function create() {
     this.physics.add.collider(player, bombs, hitBomb, null, this);
 
     cursors = this.input.keyboard.createCursorKeys();
+
+    // === MOBILE TOUCH CONTROLS ===
+    if (isMobile) {
+        const buttonRadius = 50;
+        const padding = 30;
+
+        // LEFT & RIGHT on the LEFT side
+        leftButton = this.add.circle(
+            padding + buttonRadius,
+            this.scale.height - (padding + buttonRadius),
+            buttonRadius, 0x000000, 0.5
+        ).setInteractive().setDepth(10);
+
+        rightButton = this.add.circle(
+            padding + buttonRadius * 3 + padding,
+            this.scale.height - (padding + buttonRadius),
+            buttonRadius, 0x000000, 0.5
+        ).setInteractive().setDepth(10);
+
+        leftText = this.add.text(leftButton.x, leftButton.y, '←', {
+            fontSize: '56px', color: '#ffffff'
+        }).setOrigin(0.5).setDepth(11);
+
+        rightText = this.add.text(rightButton.x, rightButton.y, '→', {
+            fontSize: '56px', color: '#ffffff'
+        }).setOrigin(0.5).setDepth(11);
+
+        // JUMP on the RIGHT side
+        jumpButton = this.add.circle(
+            this.scale.width - (padding + buttonRadius),
+            this.scale.height - (padding + buttonRadius),
+            buttonRadius * 1.3, 0x000000, 0.5  // slightly larger
+        ).setInteractive().setDepth(10);
+
+        jumpText = this.add.text(jumpButton.x, jumpButton.y, 'JUMP', {
+            fontSize: '36px', color: '#ffffff', fontFamily: 'Arial Black'
+        }).setOrigin(0.5).setDepth(11);
+
+        // Input handling
+        leftButton.on('pointerdown', () => cursors.left.isDown = true);
+        leftButton.on('pointerup', () => cursors.left.isDown = false);
+        leftButton.on('pointerout', () => cursors.left.isDown = false);
+
+        rightButton.on('pointerdown', () => cursors.right.isDown = true);
+        rightButton.on('pointerup', () => cursors.right.isDown = false);
+        rightButton.on('pointerout', () => cursors.right.isDown = false);
+
+        jumpButton.on('pointerdown', () => cursors.up.isDown = true);
+        jumpButton.on('pointerup', () => cursors.up.isDown = false);
+        jumpButton.on('pointerout', () => cursors.up.isDown = false);
+    }
+
+    // Portrait overlay
+    portraitOverlay = this.add.container(400, 300).setVisible(false);
+    const bg = this.add.rectangle(0, 0, 500, 180, 0x000000, 0.75);
+    const msg = this.add.text(0, 0, 'Please rotate to landscape mode', {
+        fontSize: '36px',
+        fill: '#ffffff',
+        align: 'center'
+    }).setOrigin(0.5);
+    portraitOverlay.add([bg, msg]);
+
+    // Resize & orientation handling
+    this.scale.on('resize', resizeControls, this);
+    this.scale.on('orientationchange', handleOrientation, this);
+
+    this.scale.lockOrientation('landscape');
+    handleOrientation.call(this);
+}
+
+function resizeControls() {
+    if (!isMobile) return;
+
+    const buttonRadius = 50;
+    const padding = 30;
+
+    // Reposition left/right
+    leftButton?.setPosition(padding + buttonRadius, this.scale.height - (padding + buttonRadius));
+    rightButton?.setPosition(padding + buttonRadius * 3 + padding, this.scale.height - (padding + buttonRadius));
+    leftText?.setPosition(leftButton.x, leftButton.y);
+    rightText?.setPosition(rightButton.x, rightButton.y);
+
+    // Reposition jump
+    jumpButton?.setPosition(this.scale.width - (padding + buttonRadius), this.scale.height - (padding + buttonRadius));
+    jumpText?.setPosition(jumpButton.x, jumpButton.y);
+
+    // Center overlay
+    portraitOverlay.setPosition(this.scale.width / 2, this.scale.height / 2);
+}
+
+function handleOrientation() {
+    if (this.scale.orientation === Phaser.Scale.PORTRAIT) {
+        this.physics.pause();
+        portraitOverlay.setVisible(true);
+    } else {
+        this.physics.resume();
+        portraitOverlay.setVisible(false);
+    }
+    resizeControls.call(this);
 }
 
 function update() {
@@ -115,5 +228,4 @@ function hitBomb(player, bomb) {
     this.physics.pause();
     player.setTint(0xff0000);
     player.anims.play('turn');
-    gameOver = true;
 }
